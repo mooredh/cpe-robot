64b4a4f317b60e49186801c6034717f5353bc4e1
Eyitayo <eyitayoogunbiyi@gmail.com>
2019-04-30 04:24:34 +0100

    implemented voice recog

--- js/index.js
+++ js/index.js
@@ -20,3 +20,34 @@ bWard.onmouseup = () => stop();
 
 fWard.onmousedown = () => move("forward");
 fWard.onmouseup = () => stop();
+
+
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+let finalTranscript = '';
+let recognition = new window.SpeechRecognition();
+recognition.interimResults = true;
+recognition.maxAlternatives = 10;
+recognition.continuous = true;
+
+recognition.onerror = (e) => {
+    console.log("Error")
+    console.log(e)
+}
+
+recognition.onresult = (event) => {
+  let interimTranscript = '';
+  for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+    let transcript = event.results[i][0].transcript;
+    if (event.results[i].isFinal) {
+      finalTranscript += transcript;
+    } else {
+      interimTranscript += transcript;
+    }
+  }
+  document.querySelector('.title').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
+}
+
+recognition.onspeechend = () => recognition.stop()
+
+
+const startRecognition = () => recognition.start();
